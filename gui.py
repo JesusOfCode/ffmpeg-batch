c069b681c8c2160710c82202042cfd11bb3ee2a0
JesusOfCode <sulinheikki@gmail.com>
2022-12-12 02:59:03 +0200

    onefile branch

--- gui.py
+++ gui.py
@@ -1,13 +1,22 @@
 from tkinter import *
 from tkinter.filedialog import *
-import auto
 import os
-
-pwd = os.getcwd()
+import subprocess
 
 def conversion():
     dir = folder.get()
-    auto.convert(dir, "mp4")
+
+    os.chdir(dir)
+    try:
+        os.mkdir("Converts")
+    except FileExistsError:
+        pass
+    
+    n = 0
+    for i in os.listdir(dir):
+        if os.path.isfile(i):
+            subprocess.run(["ffmpeg", "-i", i, f".\Converts\out{n}.{format}"])
+            n += 1
 
 def select():
     dir = askdirectory()
